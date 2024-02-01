068b7086e0480d627f142b35123ab4d731bef8cd
Lensyl Urbano <lurbano@earthsciweb.org>
2024-01-31 21:42:30 -0600

    Add files via upload

--- huggingFace/hugTest/ttsTest.py
+++ huggingFace/hugTest/ttsTest.py
@@ -1,34 +1,12 @@
-# from transformers import pipeline
-# import soundfile as sf
-
-# synthesizer = pipeline("text-to-speech", "suno/bark-small")
-
-# speech = synthesizer("Look I am generating speech in three lines of code!")
-
-# sf.write("voiceTest.wav", speech["audio"], speech["sampling_rate"])
-
-
-
-
-# from transformers import pipeline
-# import scipy
-
-# synthesiser = pipeline("text-to-speech", "suno/bark-small")
-
-# speech = synthesiser("Hello, my dog is cooler than you!", forward_params={"do_sample": True})
-
-# scipy.io.wavfile.write("bark_out.wav", rate=speech["sampling_rate"], data=speech["audio"])
-
-
-from bark import SAMPLE_RATE, generate_audio, preload_models
+from transformers import pipeline
 import soundfile as sf
 
-preload_models()
+synthesizer = pipeline("text-to-speech", "suno/bark")
 
-text_prompt = """
-     Hello, my name is Suno. And, uh — and I like pizza. [laughs] 
-     But I also have other interests such as playing tic tac toe.
-"""
-audio_array = generate_audio(text_prompt)
+speech = synthesizer("Welcome to the Makerspace! [laughs] Let's get to work.")
 
-sf.write("voiceTest.wav", audio_array, SAMPLE_RATE)
+print("Audio:", speech['audio'][0])
+print("sample rate:", speech['sampling_rate'])
+print()
+
+sf.write("voiceTest.wav", speech["audio"][0], speech["sampling_rate"])
